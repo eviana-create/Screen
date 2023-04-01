238a9b685e7523d482820f628ddfc6cbc614f41c
Emerson Viana <missonzito@outlook.com>
2023-04-01 20:42:08 -0300

    Criação do Screen

---
+++ src/Screen.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Screen extends JFrame{
+    public Screen() {
+        setVisible(true);
+        setSize(800, 500);
+        setTitle("Tela de Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+    }
+}
